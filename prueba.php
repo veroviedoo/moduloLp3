614342a6411c1ce1418c0326580e93e061140a30
deisy <oviedo.vero211@gmail.com>
2024-10-20 22:09:31 -0300

    "Added link to external stylesheet 'estilos.css' in index2.html"

---
+++ prueba.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title> 
+</head> 
+<body> 
+    <h3>Programacion JS</h3> 
+    <script> 
+        var edad = prompt("Introduzca tu edad"); 
+        if (Number(edad) >= 18) { 
+            document.write("Eres mayor de edad"); 
+        } else { 
+            document.write("Eres menor de edad");  
+        } 
+    </script> 
+</body> 
+</html>
